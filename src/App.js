dc8e688c003433e504510e9b4de1ab9f162b7546
Pragati8 <pragatikhurana8@gmail.com>
2023-03-13 22:12:17 +0530

    print preview updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import PackageForm from './Components/PackageForm';
-import Demo from './Components/Demo'
 
 function App() {
   return (
     <div className="App">
-        {/* <PackageForm /> */}
-        <Demo />
+        <PackageForm />
     </div>
   );
 }
--- src/Components/PrintPreviewPage.js
+++ src/Components/PrintPreviewPage.js
@@ -2,24 +2,26 @@ import React from 'react'
 
 const PrintPreviewPage = ({fromName, name, address,city, state1, pincode, mobileNo, packageCount}) => {
   return (
-    <div style={{fontSize: '18px'}}>
+    <div style={{fontSize: '20px'}}>
         <div style={{}}>
           To,
         </div>
         <div style={{paddingLeft: '20px'}}>
-          <div  style={{fontSize: '22px', fontWeight: '500'}}>{name}</div>
+          <div  style={{fontSize: '24px', fontWeight: '500'}}>{name}</div>
           <div>Address: {address}</div>
           <div>{city}, {state1} - {pincode}</div>
           <div> Mobile No.: {mobileNo}</div>
         </div><br />
-        <div style={{fontSize: '24px', fontWeight: '500'}}>
+        <div style={{fontSize: '26px', fontWeight: '500'}}>
             No. of Parcel: {packageCount}
         </div><br />
+
         <div>
           From,
         </div>
+        
         <div  style={{paddingLeft: '20px'}}>
-         <div  style={{fontSize: '22px', fontWeight: '500'}}>{fromName}</div>
+         <div  style={{fontSize: '24px', fontWeight: '500'}}>{fromName}</div>
          <div>Mobile No.: 9375044447</div>
          <div>Surat</div>
         </div>
